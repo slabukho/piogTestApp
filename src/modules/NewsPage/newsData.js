a8395ae8b8796201b1b2b212a732aa441df8cdcf
elektrod1234 <slabuhodanil@gmail.com>
2020-10-16 20:43:13 +0300

    change photo

--- src/modules/NewsPage/newsData.js
+++ src/modules/NewsPage/newsData.js
@@ -3,7 +3,7 @@
 export default[
     {
         title:'News 1',
-        picture:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BBC_News_%282008%29.svg/1200px-BBC_News_%282008%29.svg.png',
+        picture:'https://maneedition.mckinneychristian.org/wp-content/uploads/2018/04/news.jpg',
         text: `What is Lorem Ipsum?
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
         date:'Oct 15 2020'
